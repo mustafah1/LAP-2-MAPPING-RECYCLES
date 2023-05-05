cfbe44ac8a825daf3bf28d35771c203e49f5dbad
mustafah1 <89341847+mustafah1@users.noreply.github.com>
2023-05-06 00:16:17 +0100

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const logRoutes = require('./middleware/logger');
 const postRouter = require('./routers/post');
@@ -11,13 +12,15 @@ api.use(cors());
 api.use(express.json());
 api.use(logRoutes);
 
-api.get("/", (req, res) => {
-    res.json({
-        name: "Discretion",
-        description: "Send and receive private messages."
-    })
-})
-
+// api.get("/", (req, res) => {
+//     res.json({
+//         name: "Discretion",
+//         description: "Send and receive private messages."
+//     })
+// })
+api.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+  });
 api.use("/posts", postRouter);
 api.use("/users", userRouter);
 
