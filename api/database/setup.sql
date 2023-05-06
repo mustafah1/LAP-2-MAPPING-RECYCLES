87529e5c6bdf6e7a9f548d22870057cab3fa9d0a
jlp <mattlukin@gmail.com>
2023-05-06 14:55:14 +0100

    Seeding basic geojson data to postgres database

--- api/database/setup.sql
+++ api/database/setup.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS post;
 DROP TABLE IF EXISTS token;
 DROP TABLE IF EXISTS user_account;
+DROP TABLE IF EXISTS restaurant_locations;
 
 CREATE TABLE post (
     post_id INT GENERATED ALWAYS AS IDENTITY,
@@ -23,3 +24,24 @@ CREATE TABLE token (
     PRIMARY KEY (token_id),
     FOREIGN KEY (user_id) REFERENCES user_account("user_id")
 );
+
+CREATE TABLE restaurant_locations (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  address TEXT,
+  city TEXT,
+  state TEXT,
+  zip TEXT,
+  geom GEOMETRY(Point, 4326)
+);
+
+INSERT INTO restaurant_locations (name, address, city, state, zip, geom)
+VALUES (
+  'Example Restaurant',
+  '123 Main St',
+  'San Francisco',
+  'CA',
+  '94105',
+  ST_SetSRID(ST_MakePoint(-122.4090, 37.7837), 4326)
+);
+
