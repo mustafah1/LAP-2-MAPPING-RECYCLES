0f60e38795751f2c6bced6940f1006fa895dba05
mustafah1 <89341847+mustafah1@users.noreply.github.com>
2023-05-11 19:24:33 +0100

    Update map.js

--- client/assets/map.js
+++ client/assets/map.js
@@ -1,9 +1,9 @@
 // TO MAKE THE MAP APPEAR YOU MUST
 // ADD YOUR ACCESS TOKEN FROM
 // https://account.mapbox.com
-mapboxgl.accessToken = process.env.MAPBOX
+token = process.env.MAPBOX
 
-const map = new mapboxgl.Map({
+const map = new toke.Map({
   container: "map",
   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
   style: "mapbox://styles/mapbox/streets-v12",
