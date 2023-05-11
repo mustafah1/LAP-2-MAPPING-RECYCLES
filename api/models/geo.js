77b7871530821edca821abe1cffc641258fab3d8
jlp <mattlukin@gmail.com>
2023-05-11 17:53:37 +0100

    fixed geo.js

--- api/models/geo.js
+++ api/models/geo.js
@@ -48,43 +48,6 @@ class Geo {
 
       return idDescr
     }
-  
-  constructor(type, features) {
-    this.type = type;
-    this.data = {
-      type: "FeatureCollection",
-      features: features,
-    };
-  }
-
-  addFeature(properties, geometry) {
-    const feature = {
-      type: "Feature",
-      properties: properties,
-      geometry: geometry,
-    };
-    this.data.features.push(feature);
-  }
-
-  static async getFeatures() {
-    const response = await db.query(
-      "SELECT description, icon, ST_AsGeoJSON(geom) as geom FROM points"
-    );
-
-    let features = response.rows.map((p) => ({
-      type: "Feature",
-      properties: {
-        description: p.description,
-        icon: p.icon,
-      },
-      geometry: JSON.parse(p.geom),
-    }));
-
-    console.log(features);
-
-    return new Geo("geojson", features);
-  }
-
 }
 
 module.exports = Geo;
