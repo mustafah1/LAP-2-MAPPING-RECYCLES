bd8ec5056a7b4047a441f3429f1c8daa7a9a5c57
mustafah1 <89341847+mustafah1@users.noreply.github.com>
2023-05-12 07:29:06 +0100

    Update favourites.js

--- api/routers/favourites.js
+++ api/routers/favourites.js
@@ -1,15 +1,16 @@
 const { Router } = require('express');
 
 const favController = require('../controllers/favourites');
+const authenticator = require("../middleware/authenticator");
 
 const favRouter = Router();
 
-favRouter.get("/", favController.getData); //remove this?
-favRouter.get("/:id", favController.getFavouriteById);
-favRouter.get("/user/:id", favController.getFavouritesByUser);
-favRouter.get("/fav/:id", favController.getFavIdFromPointId);
-favRouter.post("/", favController.addFavourite);
-favRouter.delete("/:id", favController.destroy);
+favRouter.get("/", authenticator, favController.getData); //remove this?
+favRouter.get("/:id", authenticator, favController.getFavouriteById);
+favRouter.get("/user/:id", authenticator, favController.getFavouritesByUser);
+favRouter.get("/fav/:id", authenticator, favController.getFavIdFromPointId);
+favRouter.post("/", authenticator, favController.addFavourite);
+favRouter.delete("/:id", authenticator, favController.destroy);
 
 
 
