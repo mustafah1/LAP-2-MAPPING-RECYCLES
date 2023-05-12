2fd1158ac7e866ecba56c1fe0f74c91bccfc81f8
mustafah1 <89341847+mustafah1@users.noreply.github.com>
2023-05-12 07:35:27 +0100

    Update geo.js

--- api/routers/geo.js
+++ api/routers/geo.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 
 const geoController = require('../controllers/geo');
+const authenticator = require("../middleware/authenticator");
 
 const geoRouter = Router();
 
-geoRouter.get("/", geoController.getData);
-geoRouter.get("/iddescr/", geoController.getIdDescr)
+geoRouter.get("/", authenticator, geoController.getData);
+geoRouter.get("/iddescr/", authenticator, geoController.getIdDescr)
 
 
 module.exports = geoRouter;
